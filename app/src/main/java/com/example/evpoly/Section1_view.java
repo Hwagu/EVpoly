5e30bba0d9ef8279e6a32bf32fb3f5fe8cff4d95
hwagu <kjs10033@naver.com>
2022-04-22 14:37:16 +0900

    매니저 1번구역 기능 구현

--- app/src/main/java/com/example/evpoly/Section1_view.java
+++ app/src/main/java/com/example/evpoly/Section1_view.java
@@ -1,14 +1,272 @@
 package com.example.evpoly;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.ImageDecoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Section1_view extends AppCompatActivity {
+    private static final String TAG = "Mainactivity";
+    //UI버튼
+    ImageView cameraBTN;
+    ImageView resetBTN;
+    ImageView Car_image;
+    TextView carINTIME;
+    TextView carREMAIN;
+
+    //카메라 권한 요청
+    final static int TAKE_PICTURE = 1;
+    final static int REQUEST_TAKE_PHOTO = 1;
+    String mCurrentPhotoPath;
+
+    String In_time; //입차시간
+    String remain_time; //남은시간
+
+    Uri photoUri;  //사진파일 주소
+
+    //알림창
+    NotificationManager manager;
+    NotificationCompat.Builder builder;
+    private static String CHANNEL_ID = "channel1";
+    private static String CHANEL_NAME = "Channel1";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.section1_view);
+
+        cameraBTN = findViewById(R.id.cameraBTN);
+        resetBTN = findViewById(R.id.resetBTN);
+        Car_image = findViewById(R.id.Car_image);
+        carINTIME = findViewById(R.id.carINTIME);
+        carREMAIN = findViewById(R.id.carREMAIN);
+
+        //카메라 권한 설정
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "권한 설정 완료");
+            } else {
+                Log.d(TAG, "권한 설정 요청");
+                ActivityCompat.requestPermissions(Section1_view.this, new String[]{
+                        Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+
+        //onClickLister 설정
+        cameraBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.cameraBTN:
+                        Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        File photoFile = null;
+                        try {
+                            photoFile = createImageFile();
+                        } catch (IOException ex) {
+                        }
+
+                        if (photoFile != null) {
+                            photoUri = FileProvider.getUriForFile(Section1_view.this, "com.example.EVpoly.fileprovider", photoFile);
+                            i.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                            startActivityForResult(i, 0);
+                        }
+                        break;
+
+                }
+            }
+        });
+
+        resetBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick");
+                switch (v.getId()) {
+                    case R.id.resetBTN:
+                        carINTIME.setText("");
+                        carREMAIN.setText("");
+                        Car_image.setImageResource(R.drawable.empty);
+                        break;
+
+                }
+            }
+        });
+    }
+
+    // 카메라 권한 요청
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.d(TAG, "onRequestPermissionsResult");
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Permission: " + permissions[0] + "was " + grantResults[0]);
+        }
+    }
+    //카메라 촬영 저장, 이미지뷰에 보여줌
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 카메라 촬영을 하면 이미지뷰에 사진 삽입
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            File file = new File(mCurrentPhotoPath);
+            Bitmap bitmap;
+            if (Build.VERSION.SDK_INT >= 29) {
+                ImageDecoder.Source source = ImageDecoder.createSource(getContentResolver(), Uri.fromFile(file));
+                try {
+                    bitmap = ImageDecoder.decodeBitmap(source);
+                    if (bitmap != null)
+                        Car_image.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            // 이미지뷰에 파일경로의 사진을 가져와 출력
+            Car_image.setImageURI(photoUri);
+        }
+    }
+
+    // 사진 저장 , 저장되면서 시간 체크, 타이머 동작
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        mCurrentPhotoPath = image.getAbsolutePath();
+        Log.i(TAG, "path. " + mCurrentPhotoPath);
+        In_time = new SimpleDateFormat("yyyy년 MM월 dd일 HH시 mm분").format(new Date());
+        carINTIME.setText(In_time);
+        String conversionTime = "000010"; //00시 00분 00초 시간 설정
+        countDown(conversionTime);
+        return image;
+    }
+
+    //타이머 구현
+    public void countDown(String time) {
+
+        long conversionTime = 0;
+
+        // 1000 단위가 1초
+        // 60000 단위가 1분
+        // 60000 * 3600 = 1시간
+
+        String getHour = time.substring(0, 2);
+        String getMin = time.substring(2, 4);
+        String getSecond = time.substring(4, 6);
+
+        // "00"이 아니고, 첫번째 자리가 0 이면 제거
+        if (getHour.substring(0, 1) == "0") {
+            getHour = getHour.substring(1, 2);
+        }
+
+        if (getMin.substring(0, 1) == "0") {
+            getMin = getMin.substring(1, 2);
+        }
+
+        if (getSecond.substring(0, 1) == "0") {
+            getSecond = getSecond.substring(1, 2);
+        }
+
+        // 변환시간
+        conversionTime = Long.valueOf(getHour) * 1000 * 3600 + Long.valueOf(getMin) * 60 * 1000 + Long.valueOf(getSecond) * 1000;
+
+        // 첫번쨰 인자 : 원하는 시간 (예를들어 30초면 30 x 1000(주기))
+        // 두번쨰 인자 : 주기( 1000 = 1초)
+        new CountDownTimer(conversionTime, 1000) {
+
+            // 특정 시간마다 뷰 변경
+            public void onTick(long millisUntilFinished) {
+
+                // 시간단위
+                String hour = String.valueOf(millisUntilFinished / (60 * 60 * 1000));
+
+                // 분단위
+                long getMin = millisUntilFinished - (millisUntilFinished / (60 * 60 * 1000));
+                String min = String.valueOf(getMin / (60 * 1000)); // 몫
+
+                // 초단위
+                String second = String.valueOf((getMin % (60 * 1000)) / 1000); // 나머지
+
+                // 밀리세컨드 단위
+                String millis = String.valueOf((getMin % (60 * 1000)) % 1000); // 몫
+
+                // 시간이 한자리면 0을 붙인다
+                if (hour.length() == 1) {
+                    hour = "0" + hour;
+                }
+
+                // 분이 한자리면 0을 붙인다
+                if (min.length() == 1) {
+                    min = "0" + min;
+                }
+
+                // 초가 한자리면 0을 붙인다
+                if (second.length() == 1) {
+                    second = "0" + second;
+                }
+                remain_time=hour + ":" + min + ":" + second;
+                carREMAIN.setText(remain_time);
+            }
+
+            // 제한시간 종료시
+            public void onFinish() {
+
+                // 변경 후
+                remain_time = "충전이 완료되었습니다.";
+                carREMAIN.setText(remain_time);
+                showNoti();
+                // TODO : 타이머가 모두 종료될때 어떤 이벤트를 진행할지
+
+            }
+        }.start();
+
+    }
+    //타이머 종료되면 알림 구현
+    public void showNoti() {
+        builder = null;
+        manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE); //버전 오레오 이상일 경우
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            manager.createNotificationChannel(
+                    new NotificationChannel(CHANNEL_ID, CHANEL_NAME, NotificationManager.IMPORTANCE_DEFAULT));
+            builder = new NotificationCompat.Builder(this, CHANNEL_ID); //하위 버전일 경우
+        } else {
+            builder = new NotificationCompat.Builder(this);
+        }
+        //알림창 제목
+        builder.setContentTitle("EV poly");
+        //알림창 메시지
+        builder.setContentText("1번구역 충전이 완료되었습니다.");
+        //알림창 아이콘
+        builder.setSmallIcon(R.drawable.evpoli);
+        Notification notification = builder.build();
+
+        //알림창 실행
+        manager.notify(1, notification);
     }
 }
