e09e70d7e7a8144f9e4148dfc1cb315a6dc8f907
DAN <rkdekdms9918@daum.net>
2022-04-22 14:01:37 +0900

    구역 화면 세개 xml + png

---
+++ app/src/main/java/com/example/evpoly/test.java
@@ -0,0 +1,14 @@
+package com.example.evpoly;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class test extends AppCompatActivity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_main1);
+    }
+}
